576631bb11c0d28a6a786111bd795adbc29bd3be
Junior <duartejusanca@gmail.com>
2023-07-15 22:22:48 +0200

    create controller ScraperController, use the Goute package in the controller, Create an index method in the ScraperController controller, to test our installation and request

--- app/Http/Controllers/ScraperController.php
+++ app/Http/Controllers/ScraperController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Goutte\Client;
+
 
 class ScraperController extends Controller
 {
     //
+    public function index()
+    {
+        $client = new Client();
+
+        $website = $client->request('GET', 'https://laravel.com/docs/10.x/views');
+        // dd($website);
+
+        return $website->html();
+        // return view('welcome', $website->html());
+
+    }
+
 }
